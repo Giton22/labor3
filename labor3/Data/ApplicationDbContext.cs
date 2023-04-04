c663a9af114a83a543f42452d8570692a82f9c1a
giton@stud.uni-obuda.hu <giton@stud.uni-obuda.hu>
2023-04-04 13:50:41 +0200

    add users

--- labor3/Data/ApplicationDbContext.cs
+++ labor3/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using labor3.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace labor3.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        DbSet<SiteUser> Users;
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ labor3/Models/SiteUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace labor3.Models
+{
+    public class SiteUser:IdentityUser
+    {
+        public int minSalaryPerHour { get; set; }
+        public Role Role { get; set; }
+
+    }
+
+    public enum Role {ADMIN,USER }
+}
