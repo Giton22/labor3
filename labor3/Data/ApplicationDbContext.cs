d517c2c5441cba09ca6acbe307cb3b4d7c737b27
giton@stud.uni-obuda.hu <giton@stud.uni-obuda.hu>
2023-04-04 14:29:18 +0200

    update

--- labor3/Data/ApplicationDbContext.cs
+++ labor3/Data/ApplicationDbContext.cs
@@ -6,11 +6,20 @@ namespace labor3.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        DbSet<SiteUser> Users;
-        DbSet<Job> Jobs;
+        public DbSet<SiteUser> Users { get; set; }
+        public DbSet<Job> Jobs { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<SiteUser>().HasMany(j => j.Jobs).WithMany(u => u.Users);
+
+                base.OnModelCreating(builder);
+        }
+
     }
 }
