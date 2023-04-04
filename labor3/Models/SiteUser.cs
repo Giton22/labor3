ac647b9ad9f962013ec857602e6abab3dd95a1e3
giton@stud.uni-obuda.hu <giton@stud.uni-obuda.hu>
2023-04-04 14:01:22 +0200

    asd

--- labor3/Models/SiteUser.cs
+++ labor3/Models/SiteUser.cs
@@ -7,6 +7,7 @@ namespace labor3.Models
         public int minSalaryPerHour { get; set; }
         public Role Role { get; set; }
 
+       public  ICollection<Job> Jobs { get; set; }
     }
 
     public enum Role {ADMIN,USER }
