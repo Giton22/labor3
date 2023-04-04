76dfb1147d82fe6e0f753318688626dc593f9413
hallgato <hallgato@LABOR210-09>
2023-04-04 13:50:23 +0200

    job added

---
+++ labor3/Models/Job.cs
@@ -0,0 +1,11 @@
+﻿namespace labor3.Models
+{
+    public class Job
+    {
+        public string Title { get; set; }
+        public int Salary { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
